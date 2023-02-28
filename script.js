5b5536a2b3f8cca3bb51dd36ae8facf7f492cdf7
Aourid <aourid@mihad.com>
2023-02-28 13:38:25 -0500

    clean input numbers

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const firstInput = document.querySelector(".enter-first-input");
 const firstEle = document.querySelector(".input1");
 const secondEle = document.querySelector(".input2");
 const answerEle = document.querySelector(".answer");
-// const formatBy3 = document.querySelector(".format-by3");
-// const formatBy5 = document.querySelector(".format-by5");
 
 //format by 3 or by 5
 const formatBy3Ele = document.querySelector(".format-by3");
@@ -15,9 +13,15 @@ const formatByAnyValueEle = document.querySelector(".format-by-any-value");
 firstInput.addEventListener("click", (event) => {
   let num1 = firstEle.value;
   let num2 = secondEle.value;
-
-  console.log(num1);
+  console.log("numbers before cleaning", num1, ".... ", num2);
   console.log("hello");
+  if (num1[0] === "0") num1 = cleanfirstNullDigits(num1);
+  if (num2[0] === "0") num2 = cleanfirstNullDigits(num2);
+
+  console.log("numbers after cleaning", num1, ".... ", num2);
+
+  console.log("hello");
+
   if (formatBy3Ele.checked === true) {
     formatBy5Ele.checked = false;
     console.log("# is checkd");
@@ -30,9 +34,11 @@ firstInput.addEventListener("click", (event) => {
   } else {
     const formatParameter = formatByAnyValueEle.value;
     // formatByAnyValueEle
-    console.log(formatParameter);
-    firstEle.value = reformatTheNumber(num1, formatParameter);
-    secondEle.value = reformatTheNumber(num2, formatParameter);
+    console.log("paramter P:", formatParameter);
+    if (formatParameter > 1) {
+      firstEle.value = reformatTheNumber(num1, formatParameter);
+      secondEle.value = reformatTheNumber(num2, formatParameter);
+    }
   }
   console.log("**************************************");
   console.log(num1, typeof num1);
@@ -61,27 +67,37 @@ firstInput.addEventListener("click", (event) => {
     answerEle.value = reformatTheNumber(String(additionResult), 3);
   } else if (formatBy5Ele.checked === true) {
     answerEle.value = reformatTheNumber(String(additionResult), 5);
-  } else {
-    const formatParameter = formatByAnyValueEle.value;
+  } else if (formatByAnyValueEle.value > 1) {
+    // const formatParameter = formatByAnyValueEle.value;
     answerEle.value = reformatTheNumber(
       String(additionResult),
-      formatParameter
+      formatByAnyValueEle.value
     );
+  } else {
+    answerEle.value = additionResult;
+    firstEle.value = num1;
+    secondEle.value = num2;
+    console.log(" -----> num1: ", num1, "num2", num2);
   }
 
-  //   console.log(
-  //     pseudoSum(
-  //       fromStringArrayToNumberArray(num1Array),
-  //       fromStringArrayToNumberArray(num2Array)
-  //     )
-  //   );
+  // // const formatParameter = formatByAnyValueEle.value;
+  // answerEle.value = reformatTheNumber(
+  //   String(additionResult),
+  //   formatParameter
+
   event.preventDefault();
 });
 
 const stringNumberToArray = (a) => a.split("").filter((x) => x != " ");
+const fromNumberArrayToNumber = (a) => Number(a.map((x) => String(x)).join(""));
+const fromNumberArrayToString = (a) => a.map((x) => String(x)).join("");
+//*************************************************************
+// map an array of string to an arrays of numbers
+// ['1', '2', '3'] ==> [1, 2, 3]
+//*************************************************************
+const fromStringArrayToNumberArray = (a) => a.map((x) => Number(x));
 
 function reformatTheNumber(a, d) {
-  //   const b = a.split("").filter((x) => x != " ");
   const b = stringNumberToArray(a);
 
   console.log(" *** b : ", b);
@@ -90,18 +106,13 @@ function reformatTheNumber(a, d) {
   console.log(b_length, firstPart);
   let num = "";
   if (firstPart > 0) {
-    for (let i = 0; i < firstPart; i++) {
-      num += `${b[i]}`;
-    }
+    for (let i = 0; i < firstPart; i++) num += `${b[i]}`;
     num += " ";
   }
 
   for (let i = firstPart; i < b_length; i++) {
     num += `${b[i]}`;
-
-    if ((i - firstPart) % d === d - 1) {
-      num += " ";
-    }
+    if ((i - firstPart) % d === d - 1) num += " ";
   }
   console.log(num);
   return num;
@@ -113,46 +124,63 @@ function reSize(a, m) {
   return c.concat(a);
 }
 
-////////////////////////////////////////////////////
-// return pseudoSum of 2 arrays
-////////////////////////////////////////////////////
+//*************************************************************
+// Adding 2 arrays
+//*************************************************************
 function add2Arrays(a, b) {
   return a.map((x, i) => x + b[i]);
 }
 
-////////////////////////////////////////////////////
-// Transform a number into an array
-// n = 123 ==> '123' ==> ['1', '2', '3'] ==> [1, 2, 3]
-////////////////////////////////////////////////////
-const fromStringArrayToNumberArray = (a) => a.map((x) => Number(x));
-
-//Pseudo sum
+//*************************************************************
+//This is a pseudo sum. It adds 2 arrrays (inputs) and returns
+// an array as output. Ex [9, 9] + [7, 3] = [16, 12].
+// But before, it call a function reSize(array, m) to make the 2
+// input arrays same size
+// ex: a = [1, 2, 5, 0, 7], b = [9, 9] m = 5-2 = 3,
+//so b will be resized as: b = [0, 0, 0, 9, 9]
+//*************************************************************
 function pseudoSum(arr1, arr2) {
-  let arr1Length = arr1.length;
-  let arr2Length = arr2.length;
-
-  let m = arr1Length - arr2Length;
+  let m = arr1.length - arr2.length;
+  //resize the smallest(size) array
 
   arr = m > 0 ? reSize(arr2, m) : reSize(arr1, Math.abs(m));
-
   return m > 0 ? add2Arrays(arr1, arr) : add2Arrays(arr, arr2);
 }
 
-////////////////////////////////////////////////////
-// Propagate the carry and return an array
-// ////////////////////////////////////////////////////
-
+//*************************************************************
+// Propagate the carry and return an array:
+// Input is the array from pseudoSum and propagate the carry
+//*************************************************************
+// Propagate the carry
 function carryPropagation(a) {
   let carry = 0;
   let result = [];
   for (let i = a.length - 1; i >= 0; i--) {
-    console.log(` ** ${i}:  ${carry}`);
+    // console.log(` ** ${i}:  ${carry}`);
     result[i] = ((a[i] % 10) + carry) % 10;
     carry = ~~((a[i] + carry) / 10);
   }
-  result.unshift(carry);
+  console.log(" >>>>>>>>> inside carry Propagation before unshift", result);
+  if (carry !== 0) result.unshift(carry);
+  console.log(" >>>>>>>>> inside carry Propagation after unshift", result);
   return result;
 }
 
-const fromNumberArrayToNumber = (a) => Number(a.map((x) => String(x)).join(""));
-const fromNumberArrayToString = (a) => a.map((x) => String(x)).join("");
+"00 0 11 0 2"
+  .split("")
+  .filter((x) => typeof Number(x) === "number" && x !== " ");
+// ['0', '0', '0', '1', '1', '0', '2']
+
+function cleanInputNumber(n) {
+  return n.split("").filter((x) => x !== " " && x + 1 !== "0");
+}
+
+function cleanfirstNullDigits(a) {
+  let index = 0;
+  let j = 0;
+  while (a[index] === "0" && a[index + 1] === "0") {
+    j = index + 1;
+    index += 1;
+  }
+  return a.slice(j + 1);
+}
